4998b25dc25a1eed02545f10168553fa803b558d
Shatabdi Barua <u1404023@student.cuet.ac.bd>
2023-09-26 23:58:35 +0600

    added folders

---
+++ src/Controllers/adminController.js
@@ -0,0 +1,7 @@
+exports.create = async (req, res) => {res.status(200).json({status:“success”, data: “Can contain any information”})};
+
+exports.read = async (req, res) => {res.status(200).json({status:“success”, data: “Can contain any information”})};
+
+exports.delete = async (req, res) => {res.status(200).json({status:“success”, data: “Can contain any information”})};
+
+exports.update = async (req, res) => {res.status(200).json({status:“success”, data: “Can contain any information”})};
